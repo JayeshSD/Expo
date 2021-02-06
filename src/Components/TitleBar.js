a7442c607799b3f61318157d2a0b81589bbcf120
Jayesh <jd33.web@gmail.com>
2021-02-06 15:02:33 +0530

    First commit

---
+++ src/Components/TitleBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TitleBar = () => {
+  const styles = { background: "#2ecc71", height: "40px", color: "#fff" };
+  return (
+    <div style={{ background: "#2ecc71", height: "40px", color: "#fff", margin: 0 }}>
+      <h3>Users</h3>
+      
+    </div>
+  );
+};
+
+export default TitleBar;
